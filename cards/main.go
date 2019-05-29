6e82994afeeb113155c90f374c3756bb01c20cb8
Jeffrey Lowy <jeffreylowy@gmail.com>
2019-05-28 22:20:45 -0700

    [WIP] Headfirst Go

--- cards/main.go
+++ cards/main.go
@@ -1,8 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
 func main() {
 
+	fmt.Println("Package", strings.Title("Main"), "verson", rand.Intn(100012132130))
 	hand, cards := deal(newDeck(), 3)
+
+	var a = []string{"One", "Two"}
+
+	for i := range a {
+		fmt.Println(a[i])
+	}
 
 	hand.print()
 	cards.print()
